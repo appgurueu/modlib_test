3173afa07d8893a7cd768ecfe4f42a7f44bbd0a8
Lars Mueller <appgurulars@gmx.de>
2022-07-05 15:36:15 +0200

    Fix binary file reading

--- b3d.lua
+++ b3d.lua
@@ -1,6 +1,6 @@
 local mod, b3d = modlib.mod, modlib.b3d
 
-local stream = assert(io.open(mod.get_resource("modlib_test", "b3d", "character.b3d"), "r"))
+local stream = assert(io.open(mod.get_resource("modlib_test", "b3d", "character.b3d"), "rb"))
 local character = assert(b3d.read(stream))
 stream:close()
 local str = character:write_string()
